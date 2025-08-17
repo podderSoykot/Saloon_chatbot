b99679d7cc77481772d74831cd0081fc50c15ff2
podderSoykot <diptopodder95@gmail.com>
2025-08-17 16:54:47 +0600

    add home page

--- salon_chatbot/salon_chatbot/urls.py
+++ salon_chatbot/salon_chatbot/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to FIDDEN Salon Chatbot API. Use /api/chatbot/ to send messages.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
